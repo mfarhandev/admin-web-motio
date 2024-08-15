ffe100748f8bb3fd5499bbd111849dd20d17d5f6
Muhammad Farhan <111608821+mfarhandev@users.noreply.github.com>
2024-08-15 12:57:34 +0500

    yes

--- android/app/src/main/kotlin/com/example/motion_web/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.motion_web
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- lib/utils/customcolor.dart
+++
@@ -1,5 +0,0 @@
-
-
-import 'package:flutter/material.dart';
-
-Color btncolor = Color.fromRGBO(55, 145, 166, 1.0);
